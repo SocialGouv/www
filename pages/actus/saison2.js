579f46b2cc99c93037eaf1000edbdc567b8e4b12
Eric H <erich34@gmail.com>
2018-06-14 10:30:09 +0200

    amélioration de la page

--- pages/actus/saison2.js
+++ pages/actus/saison2.js
@@ -18,10 +18,10 @@ const Saison2 = () => (
       tagline="Appel à candidatures : participez !"
     />
     
-      <Section title="Devenez <span data-tip='responsable des développements techniques, biz et autres'>intrapreneur</span> d'une Startup d'État !" subTitle="Mode d'emploi" className="section-color">
-      
-      Vous êtes témoin d’irritants récurrents pour les usagers ou les agents de votre service? Vous avez une idée de solution numérique qui permettrait d’y remédier? Vous êtes prêt(e) à vous investir aux cotés d’une équipe mobilisée pendant six mois pour réussir? Vous êtes un(e) agent(e) des ministères sociaux et vous avez envie de résoudre un vrai problème ? 
-      Proposez votre idée sur cette plateforme et devenez entrepreneur(se) d'une Startup d'État !<br/>
+      <Section title={["Devenez", <span data-tip='responsable des développements techniques, biz et autres'> intrapreneur </span>, "d'une Startup d'État !"]} subTitle="Mode d'emploi" className="section-color">
+      <div className="container">
+      Vous êtes témoin d’irritants récurrents pour les usagers ou les agents de votre service? <br/>Vous avez une idée de solution numérique qui permettrait d’y remédier? Vous êtes prêt(e) à vous investir aux cotés d’une équipe mobilisée pendant six mois pour réussir? <br/>Vous êtes un(e) agent(e) des ministères sociaux et vous avez envie de résoudre un vrai problème ?<br/> 
+      <p align="center"><h3>Proposez votre idée sur <a href="#">http://incubateur.</a> et devenez entrepreneur(se) d'une Startup d'État !</h3></p><br/>
 
 
 Pour devenir entrepreneur(se) des Startups d’État, aucune expertise en informatique ou internet n’est nécessaire : seule compte votre motivation pour résoudre un véritable problème.
@@ -34,6 +34,7 @@ Vous pouvez compléter votre candidature en plusieurs fois et reprendre le formu
 <br/>
 
 Vous avez jusqu'au vendredi 7 Septembre 2018 pour déposer vos idées. Si votre idée est retenue, vous serez ensuite très vite recontacté(e) pour participer à la journée de sélection du mercredi 5 octobre 2018. Vous serez alors invité(e) à cette journée pendant laquelle vous serez accompagné(e) pour structurer votre idée et la présenter devant un jury d'experts.
+</div>      
       </Section>
       
   </Layout>      
