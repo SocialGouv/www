2206b62d7c3dc953dd4a6759b039f7ae48da31b9
Eric H <erich34@gmail.com>
2018-06-27 11:55:58 +0200

    Maj du lien vers la plateforme de dépot d'idées
    Et amélioration de la mise en page (mais ca reste moche)

--- pages/actus/saison2.js
+++ pages/actus/saison2.js
@@ -21,16 +21,16 @@ const Saison2 = () => (
       <Section title={["Devenez", <span data-tip='responsable des développements techniques, biz et autres'> intrapreneur </span>, "d'une Startup d'État !"]} subTitle="Mode d'emploi" className="section-color">
       <div className="container">
       Vous êtes témoin d’irritants récurrents pour les usagers ou les agents de votre service? <br/>Vous avez une idée de solution numérique qui permettrait d’y remédier? Vous êtes prêt(e) à vous investir aux cotés d’une équipe mobilisée pendant six mois pour réussir? <br/>Vous êtes un(e) agent(e) des ministères sociaux et vous avez envie de résoudre un vrai problème ?<br/> 
-      <p align="center"><h3><a href="https://incubateur-ministere-affaires-sociales-sante.selecteev.io/apply">Proposez votre idée</a> et devenez entrepreneur(se) d'une Startup d'État !</h3></p><br/>
+      <p align="center"><h3><a href="http://incubateur-des-ministres-sociaux-saison-2.selecteev.io/apply">Proposez votre idée</a> et devenez entrepreneur(se) d'une Startup d'État !</h3></p><br/>
 
 
-Pour devenir entrepreneur(se) des Startups d’État, aucune expertise en informatique ou internet n’est nécessaire : seule compte votre motivation pour résoudre un véritable problème.
+Pour devenir entrepreneur(se) des Startups d’État, aucune expertise en informatique ou internet n’est nécessaire : <b>seule compte votre motivation pour résoudre un véritable problème.</b>	<br/>
 
 <br/>
 <a href="https://incubateur-ministere-affaires-sociales-sante.selecteev.io/apply">Cette plateforme</a> permet de déposer vos idées. Chacune d'entre elle sera étudiée avec attention.
 <br/>
 
-Vous pouvez compléter votre candidature en plusieurs fois et reprendre le formulaire à tout moment grâce au lien que vous recevrez par mail après avoir répondu aux deux première questions.
+Vous pouvez compléter votre candidature en plusieurs fois et reprendre le formulaire à tout moment grâce au lien que vous recevrez par mail après avoir répondu aux deux première questions.<br/>
 <br/>
 
 Vous avez jusqu'au <b>vendredi 14 Septembre 2018</b> pour déposer votre idée. Si votre candidature est retenue, vous serez ensuite très vite recontacté(e) pour participer à la journée de sélection du <b>jeudi 11 octobre 2018</b>. 
