567538131abfb335b7854f7fc49c0ab9b2ac90de
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2018-08-03 10:07:15 +0200

    news: release emjpm 1.2

--- src/data/news.js
+++ src/data/news.js
@@ -1,10 +1,33 @@
 module.exports = [
   {
-    "date": "27 Juillet 2018",
-    "author": "maxime.basset",
-    "tags": ["work-in-france"],
-    "title": "Work In France: plus de 200 demandes traitées en Juillet",
-    "html": `🎉 Le service <a href="/startups/work-in-france">Work In France</a> qui dématérialise les demande d'autorisation de travail pour les étudiants étrangers,
+    "date:": "2 Aout 2018",
+    author: "adrien.gonzalez",
+    tags: ["e-mjpm"],
+    title: "e-MJPM release 1.2 « still-wildflower » 🌼",
+    html: `Nouvelle livraison de l'application e-MJPM avant la pause estivale :
+          <ul>
+            <li>Possibilité pour un mandataire de <a href="https://emjpm.num.social.gouv.fr/inscription/">s'inscrire dans l'application</a></li>
+            <li>Nouvelles fonctionnalités pour filtrer les mesures sur un fond de carte OpenStreetMap</li>
+            <li>Améliorations cosmétiques et "UX" sur les parties mandataires individuels</li>
+            <li>L'interface des tribunaux d'instance a été enrichie</li>
+            <li>Intégration de la base FINESS des établissements</li>
+            <li>admin : Gestion des utilisateurs</li>
+          </ul>
+          Côté technique, un travail de fond a été mené sur la base de données et nous avons ajouté de nombreux tests unitaires et d'intégration pour prévenir les régressions :)
+          <br><br>
+          Prochaine feature sur notre roadmap pour la rentrée 2018 : la réservation de mesures.
+          <ul>
+            <li> ➡️ <a href="/startups/e-mjpm">En savoir plus sur e-MJPM</a></li>
+            <li> ➡️ <a href="mailto:contact@emjpm.beta.gouv.fr">Contactez l'équipe</a></li>
+            <li> ➡️ <a href="https://github.com/SocialGouv/eMJPM-app/releases/tag/1.2">La release sur GitHub</a></li>
+          </ul>`
+  },
+  {
+    date: "27 Juillet 2018",
+    author: "maxime.basset",
+    tags: ["work-in-france"],
+    title: "Work In France: plus de 200 demandes traitées en Juillet",
+    html: `🎉 Le service <a href="/startups/work-in-france">Work In France</a> qui dématérialise les demande d'autorisation de travail pour les étudiants étrangers,
                 a pour la première fois dépassé les <strong>200 dossiers déposés en un mois</strong> sur la seule DR de Paris.
                 <br><br>
                 Work In France s'appuie sur le service <a href="https://demarches-simplifiees.fr">démarches simplifiées</a> pour gérer la téléprocédure
@@ -13,22 +36,22 @@ module.exports = [
                 📊 <a href="https://workinfrance.beta.gouv.fr/stats.html">dashboard public</a>`
   },
   {
-    "date": "20 Juillet 2018",
-    "author": "yann-fanch.madaule",
-    "tags": ["eig", "code-du-travail-numerique"],
-    "title":
+    date: "20 Juillet 2018",
+    author: "yann-fanch.madaule",
+    tags: ["eig", "code-du-travail-numerique"],
+    title:
       "Code du travail numérique : notre défi lauréat du programme EIG 2018 !",
-    "html": `🎉 Le <a href="/startups/code-du-travail-numerique">code du travail numérique</a> a le privilège de faire partie des
+    html: `🎉 Le <a href="/startups/code-du-travail-numerique">code du travail numérique</a> a le privilège de faire partie des
       <a href="https://www.etalab.gouv.fr/entrepreneur-e-dinteret-general-3eme-promotion-decouvrez-les-15-defis-laureats">15 défis lauréats du jury</a>
       pour participer au programme "Entrepreneur d'intérêt général" 2018. <br><br>
       Notre équipe pourra donc bénéficier <strong>pendant 10 mois</strong> de deux spécialistes de la data science et du traitement du language naturel pour améliorer les résultats de notre service.`
   },
   {
-    "date": "27 Juin 2018",
-    "author": "marc.hertzog",
-    "tags": ["code-du-travail-numerique"],
-    "title": "Code du travail numérique : nouveau prototype",
-    "html": `
+    date: "27 Juin 2018",
+    author: "marc.hertzog",
+    tags: ["code-du-travail-numerique"],
+    title: "Code du travail numérique : nouveau prototype",
+    html: `
       Une nouvelle version du prototype a été déployée sur <a href="https://code-du-travail.beta.gouv.fr">code-du-travail.beta.gouv.fr</a>.
       <br><br>
       Cette version utilise un moteur Elastic Search qui indexe les textes du code du travail, des fiches pratiques, FAQ... et utilise des techniques avancées d'analyse et matching de textes.
@@ -39,22 +62,22 @@ module.exports = [
       `
   },
   {
-    "date": "20 Juin 2018",
-    "author": "julien.bouquillon",
-    "tags": [],
-    "title": "Outil de standup sur GitHub",
-    "html": `Nous avons <a href="https://github.com/SocialGouv/standup">publié sur GitHub</a> un clone du <a href="https://github.com/betagouv/standup">projet original</a>, adapté pour servir de timer à notre rituel de standup hebdomadaire (Le mardi à midi).
+    date: "20 Juin 2018",
+    author: "julien.bouquillon",
+    tags: [],
+    title: "Outil de standup sur GitHub",
+    html: `Nous avons <a href="https://github.com/SocialGouv/standup">publié sur GitHub</a> un clone du <a href="https://github.com/betagouv/standup">projet original</a>, adapté pour servir de timer à notre rituel de standup hebdomadaire (Le mardi à midi).
             <br><br>
             Pour ajouter un sujet au prochain standup, il suffit de <a href="https://github.com/SocialGouv/standup/edit/master/src/startups.json">soumettre une pull request</a> sur la page du projet.
       `
   },
   {
-    "date": "18 Janvier 2018",
-    "author": "julien.bouquillon",
-    "tags": ["code-du-travail-numerique"],
-    "title":
+    date: "18 Janvier 2018",
+    author: "julien.bouquillon",
+    tags: ["code-du-travail-numerique"],
+    title:
       "Code du travail numérique : publication des 50 questions les plus fréquentes",
-    "html":
-      "Une F.A.Q. qui reprend les 50 questions les plus fréquemment posées vient d'être publiée par les services de renseignement et aussitôt mise en ligne : <a href=\"https://socialgouv.github.io/faq-code-du-travail\">socialgouv.github.io/faq-code-du-travail</a>"
+    html:
+      'Une F.A.Q. qui reprend les 50 questions les plus fréquemment posées vient d\'être publiée par les services de renseignement et aussitôt mise en ligne : <a href="https://socialgouv.github.io/faq-code-du-travail">socialgouv.github.io/faq-code-du-travail</a>'
   }
-]
+];
