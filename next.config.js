2a322d61b758a18fe4cc63e439feea3ff5232149
Douglas Duteil <douglasduteil@gmail.com>
2018-10-02 18:22:35 +0200

    feat(pkg): get template.data.gouv.fr css style from npm (#46)

--- next.config.js
+++ next.config.js
@@ -1,17 +1,29 @@
-const withCSS = require("@zeit/next-css");
+const withCSS = require('@zeit/next-css');
+const withImages = require('next-images');
 
-const withMDX = require("@zeit/next-mdx")({
+const withMDX = require('@zeit/next-mdx')({
   options: {
     mdPlugins: [
-      require("remark-autolink-headings"),
-      require("remark-highlight.js")
+      require('remark-autolink-headings'),
+      require('remark-highlight.js')
     ]
   }
 });
 
 module.exports = withMDX(
-  withCSS({
-    pageExtensions: ["js", "mdx"],
-    assetPrefix: ""
-  })
+  withImages(
+    withCSS({
+      pageExtensions: ['js', 'mdx'],
+      assetPrefix: '',
+
+      webpack: config => {
+        config.module.rules.push({
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: 'file-loader?name=static/fonts/[name].[ext]'
+        });
+
+        return config;
+      }
+    })
+  )
 );
