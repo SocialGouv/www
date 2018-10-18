9bda64ed1749af0596f4500c934075f0a468a314
Eric H <erich34@gmail.com>
2018-10-18 10:51:11 +0200

    Ajout de la news sur les nouveaux lauréats

--- pages/actus/jour-j.js
+++ pages/actus/jour-j.js
@@ -18,63 +18,43 @@ const JourJ = () => (
       className="section-color"
     >
       <div className="container">
-        Au terme d'une journée intensive, il n'en est resté que deux.
-        <br /> Tout a commencé autour d'un petit déjeuner bio. Sur la dizaine de
-        candidats pré-sélectionnés, c'est au final huit candidats qui sont venus
-        le 11 octobre chez notre partenaire le Numa, au coeur de Paris. Les
-        idées présentées furent les suivantes : <br />- Rendre plus performant
-        le processus de domiciliation des personnes sans domicile stable <br />-
-        La création d'un guichet d'inscription aux formation qualifiantes du
-        ministère des sports. <br />- Aider les services de l'état à mettre
-        rapidement à l'abris les personnes à la rue durant l'hiver. <br />-
-        Mettre en valeur les territoires déficitaires en professionels de santé
-        afin de favoriser leur installation <br />- "Dites le nous une fois"
-        pour les entreprises étrangères détachant des salariés en France <br />-
-        Outiller les travailleurs sociaux pour accompagner les exclus numériques{" "}
-        <br />- Stimuler les jeunes dans leurs démarches tout au long de leur
-        parcours afin d'assurer un meilleur suivi par les DIRECCTE et les
-        conseillers Mission Locale. <br />- Améliorer l’accès à l’offre de soins
-        en cas d'urgence.
-        <br /> La matinée et le début d'après-midi ont été consacrés à la
-        présentation des pitchs (voir encadré).
-        <br /> Puis, à 15h, les présentations officielles ont commencé devant un
-        jury constitué des experts suivants :<br /> - Hélène Brisset, directrice
-        du système d'information d'information des ministères sociaux <br />-
-        Vincent Gaillot, <br />- Jasmine Meurin, intrapreneur de la saison 1 sur
-        le produit e-mjpm <br />- John Redt- , intrapreneur de la saison 1 sur
-        le produit workInFrance <br />- ?? , entrepreneur chez MakeSense <br />-
-        ??, entrepreneur chez Coding <br />Et il faut reconnaitre que les
-        orateurs avaient du talent ! De leur propre aveux, le choix du jury
-        s'averaient complexe. Mais l'arrivée de la secrétaire générale, Madame
-        Sabine Fourcade, a permis de faire les choix. C'est au final X et Y qui
-        remporté la saison 2 et se voient donc devenir les intrapreneurs de leur
-        startup au sein de notre incubateur. Bravo à tous les participants de la
-        journée pour leur dynamisme et leur enthousiasme. Et à très bientôt pour
-        avoir des nouvelles de nos nouveaux "intras" !
+        Au terme d'une journée intensive, ce ne sont pas deux mais trois projets
+        qui ont été choisis !<br /> Tout a commencé autour d'un petit déjeuner
+        bio. Sur la dizaine de candidats pré-sélectionnés, c'est au final huit
+        candidats qui sont venus le 11 octobre chez notre partenaire le Numa, au
+        coeur de Paris. <br />
+        <br />Les idées présentées furent les suivantes : <br />- Rendre plus
+        performant le processus de domiciliation des personnes sans domicile
+        stable <br />- La création d'un guichet d'inscription aux formation
+        qualifiantes du ministère des sports. <br />- Aider les services de
+        l'état à mettre rapidement à l'abris les personnes à la rue durant
+        l'hiver. <br />- Mettre en valeur les territoires déficitaires en
+        professionels de santé afin de favoriser leur installation <br />-
+        "Dites le nous une fois" pour les entreprises étrangères détachant des
+        salariés en France <br />- Outiller les travailleurs sociaux pour
+        accompagner les exclus numériques <br />- Stimuler les jeunes dans leurs
+        démarches tout au long de leur parcours afin d'assurer un meilleur suivi
+        par les DIRECCTE et les conseillers Mission Locale. <br />- Améliorer
+        l’accès à l’offre de soins en cas d'urgence.
+        <br />
+        <br /> La matinée et le début d'après-midi ont été consacrés à un
+        coaching des candidats puis à la présentation des pitchs. Puis, à 15h,
+        les présentations officielles ont commencé devant un jury constitué
+        d'experts : Jasmine Meurin, intrapreneur de la saison 1 sur le produit
+        e-mjpm, Johnathan Redt-Gensinger , intrapreneur de la saison 1 sur le
+        produit workInFrance, Basile Michardière (Makesense.org) et Julie
+        Jouvencel (coding-days.com) deux experts du secteur numérique, Vincent
+        Gaillot, directeur de projet auprès de la secrétaire générale chef du
+        pôle modernisation et Hélène Brisset, notre directrice.<br /> Les
+        orateurs ont eu beaucoup de talent ! C'est au final les projets 1, 3 et
+        4 qui ont remporté l'adhésion du jury. <br />
+        <br />A très bientôt pour avoir des nouvelles de nos nouveaux "intras" !<br />
+        <br />
+        <br />
         <div style={{ textAlign: "center" }}>
-          <img src="/static/images/aventure.jpg" width="50%" />
+          <img src="/static/images/pitchday 1.jpeg" width="70%" />
         </div>
         <br />
-        <a
-          href="http://incubateur-des-ministres-sociaux-saison-2.selecteev.io/apply"
-          target="_blank"
-        >
-          Cette plateforme
-        </a>{" "}
-        permet de déposer vos idées. Chacune d'entre elles sera étudiée avec
-        attention.
-        <br />
-        Vous pouvez compléter votre candidature en plusieurs fois et reprendre
-        le formulaire à tout moment grâce au lien que vous recevrez par mail
-        après avoir répondu aux deux premières questions.<br />
-        <br />
-        Vous avez jusqu'au <b>lundi 24 Septembre 2018</b> pour déposer votre
-        idée. Si votre candidature est retenue, vous serez ensuite très vite
-        recontacté(e) pour participer à la journée de sélection du{" "}
-        <b>jeudi 11 octobre 2018</b>. Vous participerez alors à un bootcamp
-        pendant lequel vous serez coaché(e) pour{" "}
-        <span data-tip="Présenter de façon très synthétique">pitcher</span>{" "}
-        devant un jury d'experts.
       </div>
     </Section>
   </Layout>
--- src/composants/AllActus.js
+++ src/composants/AllActus.js
@@ -1,6 +1,17 @@
 import { Card } from ".";
 
 // Ajouter les actus ici
+
+const JourJ = () => (
+  <Card
+    href="/actus/jour-j"
+    img="/static/images/pitchday 1.jpeg"
+    title="Saison 2 : le jour J"
+    meta="Octobre 2018"
+    description="Découvrez les idées et les lauréats de la saison 2 de l'incubateur"
+  />
+);
+
 const CardActuSaison2 = () => (
   <Card
     href="/actus/saison2"
@@ -35,8 +46,7 @@ const CardActuBoard1 = () => (
 const AllActus = {
   actuSaison2: CardActuSaison2,
   actuCdtn1: CardActuCDTN1,
-  actuBoard1: CardActuBoard1
+  jourj: JourJ
 };
 
 export default AllActus;
- 
