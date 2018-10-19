b7a040ea7c13b259429ffbd65428a7b0f8a93090
Eric H <erich34@gmail.com>
2018-10-19 11:48:46 +0200

    maj de la news
    ajout de la mention de la SG et d'une image

--- pages/actus/saison-2-jour-j.js
+++ pages/actus/saison-2-jour-j.js
@@ -22,38 +22,44 @@ const JourJ = () => (
         c&apos;est au final huit candidats qui sont venus le 11 octobre chez
         notre partenaire le Numa, au coeur de Paris. <br />
         <br />
-        Les idées présentées furent les suivantes : <br />- Rendre plus
+        Les idées présentées furent les suivantes : <br />1- Rendre plus
         performant le processus de domiciliation des personnes sans domicile
-        stable <br />- La création d&apos;un guichet d&apos;inscription aux
-        formation qualifiantes du ministère des sports. <br />- Aider les
+        stable <br />2- La création d&apos;un guichet d&apos;inscription aux
+        formation qualifiantes du ministère des sports. <br />3- Aider les
         services de l&apos;état à mettre rapidement à l&apos;abris les personnes
-        à la rue durant l&apos;hiver. <br />- Mettre en valeur les territoires
+        à la rue durant l&apos;hiver. <br />4- Mettre en valeur les territoires
         déficitaires en professionels de santé afin de favoriser leur
-        installation <br />- "Dites le nous une fois" pour les entreprises
-        étrangères détachant des salariés en France <br />- Outiller les
-        travailleurs sociaux pour accompagner les exclus numériques <br />-
+        installation <br />5- "Dites le nous une fois" pour les entreprises
+        étrangères détachant des salariés en France <br />6- Outiller les
+        travailleurs sociaux pour accompagner les exclus numériques <br />7-
         Stimuler les jeunes dans leurs démarches tout au long de leur parcours
         afin d&apos;assurer un meilleur suivi par les DIRECCTE et les
-        conseillers Mission Locale. <br />- Améliorer l’accès à l’offre de soins
+        conseillers Mission Locale. <br />8- Améliorer l’accès à l’offre de soins
         en cas d&apos;urgence.
         <br />
         <br /> La matinée et le début d&apos;après-midi ont été consacrés à un
-        coaching des candidats puis à la présentation des pitchs. Puis, à 15h,
-        les présentations officielles ont commencé devant un jury constitué
-        d&apos;experts : Jasmine Meurin, intrapreneur de la saison 1 sur le
+        coaching des candidats puis à la présentation des pitchs.
+        <div style={{ textAlign: "center" }}>
+          <img src="/static/images/pitch.jpg" width="40%" /><br/>
+          
+        </div>
+        <br/> Puis, à 15h,
+        les présentations officielles ont commencé devant un jury
+        d&apos;experts présidé par la secrétaire générale, Mme Sabine Fourcade accompagnée de Jasmine Meurin, intrapreneur de la saison 1 sur le
         produit e-mjpm, Johnathan Redt-Gensinger , intrapreneur de la saison 1
         sur le produit workInFrance, Basile Michardière (Makesense.org) et Julie
         Jouvencel (coding-days.com) deux experts du secteur numérique, Vincent
         Gaillot, directeur de projet auprès de la secrétaire générale chef du
-        pôle modernisation et Hélène Brisset, notre directrice.
-        <br /> Les orateurs ont eu beaucoup de talent ! C&apos;est au final les
+        pôle modernisation et Hélène Brisset, directrice des systèmes d'information.
+        <br /> Les orateurs ont eu beaucoup de talent ! Le choix fut compliqué mais c&apos;est au final les
         projets 1, 3 et 4 qui ont remporté l&apos;adhésion du jury. <br />
         <br />A très bientôt pour avoir des nouvelles de nos nouveaux "intras" !
         <br />
         <br />
         <br />
         <div style={{ textAlign: "center" }}>
-          <img src="/static/images/pitchday-1.jpg" width="70%" />
+          <img src="/static/images/pitchday-1.jpg" width="70%" /><br/>
+          <i>Les participants de cette belle journée</i>
         </div>
         <br />
       </div>
