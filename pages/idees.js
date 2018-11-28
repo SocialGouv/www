50a024d5a242f0bee606c2f77ad2d29a4be35e28
Eric H <erich34@gmail.com>
2018-11-28 16:52:30 +0100

    ajout du package permetttant d'embed du typeform
    necessite de faire un npm install

--- pages/idees.js
+++ pages/idees.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ReactTypeformEmbed } from "react-typeform-embed";
 
 import { Section, Layout, AllActus, Hero } from "../src/composants";
 import news from "../src/data/news";
@@ -41,15 +42,19 @@ const Actus = () => (
       className="section-white"
       title="Vous êtes témoin d’irritants récurrents pour les usagers ou les agents de votre service ? "
       subTitle="Vous avez une idée de solution numérique qui permettrait d’y remédier ?"
-    >
-     <div class="typeform-widget" data-url="https://incubateurministeressociaux.typeform.com/to/BNlCIL" data-transparency="50" style="width: 100%; height: 500px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=BNlCIL&utm_source=typeform.com-12986254-Basic&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=EN" style="color: #999" target="_blank">Typeform</a> </div>
-    </Section>
-    !!!Ou ici !!!
-    <Section
-      className="section-color cards"
-      title="Articles de fond"
-      subTitle=""
     />
+
+    <Section className="section-color cards" title=" - " subTitle="">
+      <ReactTypeformEmbed
+        url={"https://incubateurministeressociaux.typeform.com/to/BNlCIL"}
+        buttonText="Déposer mon idée"
+        style={"height: 300px; width: 50%; "}
+        autoOpen="false"
+        hideHeaders="true"
+        hideFooter="true"
+        opacity="50"
+      />
+    </Section>
   </Layout>
 );
 
