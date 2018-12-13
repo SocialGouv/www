e6e649a2b6df9f684e08901094f40ffcefe98fa2
Douglas Duteil <douglasduteil@gmail.com>
2018-12-13 12:06:11 +0100

    test(e2e): allow e2e tests to be 10s slow

--- optional/e2e/__test__/index.spec.ts
+++ optional/e2e/__test__/index.spec.ts
@@ -20,6 +20,8 @@ const paths = [
   ["/startups/work-in-france"]
 ];
 
+jest.setTimeout(10 * 1000) // allow to be 10s slow
+
 test.each(paths)("%s should be reachable", async path => {
   await page.goto(process.env.BASE_URL + path);
 });
