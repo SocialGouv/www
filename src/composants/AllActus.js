6f4d6f802d409aa0d68e13bc860da30cf3a12a68
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-04-04 10:23:04 +0200

    chore: update rss script

--- src/composants/AllActus.js
+++ src/composants/AllActus.js
@@ -1,38 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Card } from ".";
-
 import news from "../data/news";
-
-// const AllActus = ({ count = 0 }) => (
-//   <div
-//     className="row cards"
-//     style={{
-//       color: "black",
-//       flexWrap: "wrap",
-//       justifyContent: "space-between"
-//     }}
-//   >
-//     {news.slice(0, count ? count : news.length).map(actu => (
-//       <Card
-//         key={actu.title}
-//         {...actu}
-//         style={{ flex: "1 0 auto", margin: "5px auto" }}
-//       />
-//     ))}
-//   </div>
-// );
-
-// AllActus.propTypes = {
-//   count: PropTypes.number
-// };
-
-//import news from "../src/data/news.js";
 
 // https://github.com/facebook/react/issues/377
 const News = ({ title, href, date, html }) => (
-  <div className="row">
+  <div className="row" name={date}>
     <div
       className="panel"
       style={{
