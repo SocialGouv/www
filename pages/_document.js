91eb48b9e71286078bba7a6f6ed5a1c673aab5dc
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-03-23 15:08:50 +0100

    chore: rm polyfill service (#607)

--- pages/_document.js
+++ pages/_document.js
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, user-scalable=yes"
           />
-          <script src="//polyfill.incubateur.social.gouv.fr/v3/polyfill.min.js?flags=gated&features=fetch%2CArray.prototype.find" />
+          <script src="/polyfill.js" />
           {this.props.styleTags}
         </Head>
         <body>
