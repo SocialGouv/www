c518050e323323d5abed09a5005add7c0f21f345
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-01-21 12:08:02 +0100

    Use Harbor in Kosko config.

---
+++ .k8s/components/www.ts
@@ -0,0 +1,30 @@
+import env from "@kosko/env";
+
+import { create } from "@socialgouv/kosko-charts/components/app";
+import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
+
+const IMAGE_TAG = process.env.CI_COMMIT_TAG
+  ? process.env.CI_COMMIT_TAG.replace(/^v/, "")
+  : process.env.CI_COMMIT_SHA;
+
+const manifests = create("www", {
+  env,
+  config: { containerPort: 3000 },
+  deployment: {
+    image: getHarborImagePath({ name: "socialgouv" }),
+    container: {
+      resources: {
+        requests: {
+          cpu: "100m",
+          memory: "128Mi"
+        },
+        limits: {
+          cpu: "500m",
+          memory: "1280Mi"
+        }
+      }
+    }
+  }
+});
+
+export default manifests;
