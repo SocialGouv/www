750c60ebcdac8045694eada83991199855189888
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-01-22 17:59:35 +0100

    Update .k8s/components/www.ts
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

--- .k8s/components/www.ts
+++ .k8s/components/www.ts
@@ -3,9 +3,6 @@ import env from "@kosko/env";
 import { create } from "@socialgouv/kosko-charts/components/nginx";
 import { getHarborImagePath } from "@socialgouv/kosko-charts/utils/getHarborImagePath";
 
-const IMAGE_TAG = process.env.CI_COMMIT_TAG
-  ? process.env.CI_COMMIT_TAG.replace(/^v/, "")
-  : process.env.CI_COMMIT_SHA;
 
 const manifests = create("www", {
   env,
