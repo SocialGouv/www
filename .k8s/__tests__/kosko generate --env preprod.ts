5afbc433edb39554858d19243dab0aacaf316129
Douglas Duteil <douglasduteil@gmail.com>
2021-02-22 12:03:15 +0100

    test(k8s): add missing HARBOR_PROJECT var env (#813)
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

--- .k8s/__tests__/kosko generate --env preprod.ts
+++ .k8s/__tests__/kosko generate --env preprod.ts
@@ -6,6 +6,7 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 jest.setTimeout(1000 * 60);
 test("kosko generate --preprod", async () => {
   process.env.RANCHER_PROJECT_ID = "c-kk8xm:p-s5gkl";
+  process.env.HARBOR_PROJECT = "fabrique";
   expect(
     await getEnvManifests("preprod", "", project("www").preprod)
   ).toMatchSnapshot();
