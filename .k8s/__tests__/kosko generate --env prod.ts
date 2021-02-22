fcbce573093da28dde32b5b2b58a754071bc26da
Douglas DUTEIL <douglasduteil@gmail.com>
2021-02-22 11:30:07 +0100

    test(k8s): add missing HARBOR_PROJECT var env

--- .k8s/__tests__/kosko generate --env prod.ts
+++ .k8s/__tests__/kosko generate --env prod.ts
@@ -5,6 +5,7 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
+  process.env.HARBOR_PROJECT = "fabique";
   expect(
     await getEnvManifests("prod", "'!(_*)'", project("www").prod)
   ).toMatchSnapshot();
