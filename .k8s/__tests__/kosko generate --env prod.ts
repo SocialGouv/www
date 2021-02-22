35f61b58230c3324fa8105adb616abede5b598b7
Douglas Duteil <douglasduteil@gmail.com>
2021-02-22 11:51:33 +0100

    Update .k8s/__tests__/kosko generate --env prod.ts
    Co-authored-by: Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>

--- .k8s/__tests__/kosko generate --env prod.ts
+++ .k8s/__tests__/kosko generate --env prod.ts
@@ -5,7 +5,7 @@ import { project } from "@socialgouv/kosko-charts/testing/fake/gitlab-ci.env";
 
 jest.setTimeout(1000 * 60);
 test("kosko generate --prod", async () => {
-  process.env.HARBOR_PROJECT = "fabique";
+  process.env.HARBOR_PROJECT = "fabrique";
   expect(
     await getEnvManifests("prod", "'!(_*)'", project("www").prod)
   ).toMatchSnapshot();
