0d32515dcca40bff77cdfecb24eb07bd84c797f0
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-05-12 15:19:31 +0200

    fix: minors and headers (#836)
    * fix: use matomo-next
    
    * fix(ui): fix marianne width
    
    * feat(ingress): add security headers
    
    * fix(ci): upgrade gitlab-ci-yml

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,15 @@ import App from "next/app";
 import Head from "next/head";
 import React from "react";
 
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || "https://matomo.fabrique.social.gouv.fr";
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || 2;
+
 export default class MyApp extends App {
+  componentDidMount() {
+    init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
+  }
   render() {
     const { Component, pageProps } = this.props;
 
--- src/composants/Layout.js
+++ src/composants/Layout.js
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import React from "react";
 import ReactTooltip from "react-tooltip";
 
-import { ReactPiwik } from "../piwik";
 import { Footer, GenericLink, Header, SvgIcons } from ".";
 
 const components = {
@@ -49,33 +48,6 @@ const footerLinks = [
 ];
 
 export default class Layout extends React.Component {
-  /*
-    for some reason, Link to internal anchors "#" doesnt work properly, at least in dev
-    this `componentDidMount` enforces systematic anchor scroll on page change
-  */
-  componentDidMount() {
-    const isBrowser = typeof document !== undefined;
-    if (isBrowser) {
-      ReactPiwik.push(["setCustomUrl", document.location.href]);
-      ReactPiwik.push(["setDocumentTitle", document.title]);
-      ReactPiwik.push(["trackPageView"]);
-
-      const url = document.location.href;
-      const pattern = /^(\/#.+)|(.+(\/#.+))$/;
-      if (url && pattern.test(url)) {
-        const hash = pattern
-          .exec(url)
-          .filter((item) => item)
-          .pop();
-        const id = hash.replace(/\/?#/, "");
-        const target = document.getElementById(id);
-        if (target) {
-          const top = target.getBoundingClientRect().top;
-          setTimeout(window.scroll(0, top));
-        }
-      }
-    }
-  }
   render() {
     return (
       <React.Fragment>
