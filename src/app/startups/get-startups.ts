aefe1eb714250bcf662082207d644eed5d5a84e7
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-08-10 20:20:46 +0200

    feat: add startups list (#1030)

---
+++ src/app/startups/get-startups.ts
@@ -0,0 +1,25 @@
+import json from "./startups.json"
+
+export default function getStartups() {
+  const { data } = json
+  const excludedPhases = ["transfer", "alumni"]
+
+  return data.reduce(
+    (startups, startup) => {
+      const {
+        attributes: { phases },
+      } = startup
+
+      if (
+        startup.type === "startup" &&
+        startup.relationships.incubator.data.id === "sgmas" &&
+        !phases.some((phase) => excludedPhases.includes(phase.name))
+      ) {
+        startups.push(startup)
+      }
+
+      return startups
+    },
+    [] as typeof json.data
+  )
+}
