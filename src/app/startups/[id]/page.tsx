2976b7221791cd6674a55e31c9e26fcb96e4394f
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-08-11 04:57:38 +0200

    feat: add markdown text to startup details page (#1032)

--- src/app/startups/[id]/page.tsx
+++ src/app/startups/[id]/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image"
-// import { remark } from "remark"
+import { remark } from "remark"
 import { fr } from "@codegouvfr/react-dsfr"
-// import type { MDXComponents } from "mdx/types"
-// import { MDXRemote } from "next-mdx-remote/rsc"
+import type { MDXComponents } from "mdx/types"
+import { MDXRemote } from "next-mdx-remote/rsc"
 
 import getStartup from "./get-startup"
 import getStartups from "../get-startups"
@@ -34,29 +34,32 @@ export default async function Details({
     content_url_encoded_markdown,
   } = attributes
 
-  // const markdown = await remark().process(
-  //   decodeURIComponent(content_url_encoded_markdown)
-  // )
+  const markdown = await remark().process(
+    decodeURIComponent(content_url_encoded_markdown)
+  )
 
-  // const component = {
-  //   h2: ({ children }: { children: React.ReactNode }) => (
-  //     <h2 className="fr-h2">{children}</h2>
-  //   ),
-  //   h3: ({ children }: { children: React.ReactNode }) => (
-  //     <h2 className="fr-h3">{children}</h2>
-  //   ),
-  //   p: ({ children }: { children: React.ReactNode }) => (
-  //     <p className="mb-6">{children}</p>
-  //   ),
-  //   ul: ({ children }: { children: React.ReactNode }) => (
-  //     <ul className="mb-6">{children}</ul>
-  //   ),
-  //   a: ({ children, ...props }: { children: React.ReactNode }) => (
-  //     <a {...props} target="_blank" rel="noopener">
-  //       {children}
-  //     </a>
-  //   ),
-  // } as MDXComponents
+  const component = {
+    h1: ({ children }: { children: React.ReactNode }) => (
+      <h1 className="fr-h1">{children}</h1>
+    ),
+    h2: ({ children }: { children: React.ReactNode }) => (
+      <h2 className="fr-h2">{children}</h2>
+    ),
+    h3: ({ children }: { children: React.ReactNode }) => (
+      <h2 className="fr-h3">{children}</h2>
+    ),
+    p: ({ children }: { children: React.ReactNode }) => (
+      <p className="mb-6">{children}</p>
+    ),
+    ul: ({ children }: { children: React.ReactNode }) => (
+      <ul className="mb-6">{children}</ul>
+    ),
+    a: ({ children, ...props }: { children: React.ReactNode }) => (
+      <a {...props} target="_blank" rel="noopener">
+        {children}
+      </a>
+    ),
+  } as MDXComponents
 
   const theme = fr.getColors(false)
   const blueCumulus = theme.decisions.background.alt.blueCumulus.default
@@ -101,11 +104,11 @@ export default async function Details({
           </div>
         </div>
       </div>
-      {/* <div className="fr-py-6w">
+      <div className="fr-py-6w">
         <div className="fr-container">
           <MDXRemote source={markdown} components={component} />
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
