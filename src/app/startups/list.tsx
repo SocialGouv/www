91cda70b1fbf60ba52cb1d47a1a75de8326a9532
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-08-11 19:02:31 +0200

    feat: show phase in each startups list item

--- src/app/startups/list.tsx
+++ src/app/startups/list.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@codegouvfr/react-dsfr/Card"
+import { Badge } from "@codegouvfr/react-dsfr/Badge"
 
 import getStartups from "./get-startups"
 
@@ -6,18 +7,34 @@ export default function List() {
   const startups = getStartups()
 
   return (
-    <div className="startups-list grid grid-cols-4 gap-6">
-      {startups.map(({ id, attributes: { name, pitch } }) => (
-        <Card
-          key={id}
-          enlargeLink
-          title={name}
-          desc={pitch}
-          linkProps={{ href: `/startups/${id}` }}
-          imageAlt="image d'illustration de la startup"
-          imageUrl={`https://beta.gouv.fr/img/startups/${id}.png`}
-        />
-      ))}
+    <div className="startups-list grid grid-cols-4 gap-6 fr-mt-6w">
+      {startups.map(({ id, attributes: { name, pitch, phases } }) => {
+        const phase = phases.pop()?.name
+        const severity = phase === "success" ? "success" : "info"
+
+        return (
+          <Card
+            key={id}
+            enlargeLink
+            title={name}
+            desc={pitch}
+            linkProps={{ href: `/startups/${id}` }}
+            imageAlt="image d'illustration de la startup"
+            imageUrl={`https://beta.gouv.fr/img/startups/${id}.png`}
+            start={
+              <ul className="fr-badges-group">
+                {phase && (
+                  <li>
+                    <Badge key={1} noIcon severity={severity} small>
+                      {phase === "success" ? "succès" : phase}
+                    </Badge>
+                  </li>
+                )}
+              </ul>
+            }
+          />
+        )
+      })}
     </div>
   )
 }
