033c09add286ae45edd27ae822275790214c47d2
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-08-25 18:34:52 +0200

    feat: add call for ideas component (#1041)

--- src/components/random-startups.tsx
+++ src/components/random-startups.tsx
@@ -7,12 +7,16 @@ export default function RandomStartups() {
   const randomStartups = getRandomEntries(startups, 4)
 
   return (
-    <div className="random-startups fr-container">
-      <h2 className="fr-h2">Découvrez d&apos;autres startups de la Fabrique</h2>
-      <div className="grid grid-cols-4 gap-6 fr-mt-6w">
-        {randomStartups.map((startup) => (
-          <Item key={startup.id} startup={startup} />
-        ))}
+    <div className="random-startups fr-pb-6w">
+      <div className="fr-container">
+        <h2 className="fr-h2">
+          Découvrez d&apos;autres startups de la Fabrique
+        </h2>
+        <div className="grid grid-cols-4 gap-6 fr-mt-6w">
+          {randomStartups.map((startup) => (
+            <Item key={startup.id} startup={startup} />
+          ))}
+        </div>
       </div>
     </div>
   )
