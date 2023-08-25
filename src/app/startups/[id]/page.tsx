ede3db8cdc8a9004ed8f97fa547226a3c5f770b6
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-08-25 11:41:31 +0200

    feat: add startup mdx (#1039)

--- src/app/startups/[id]/page.tsx
+++ src/app/startups/[id]/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image"
-import { remark } from "remark"
+
 import { fr } from "@codegouvfr/react-dsfr"
-import type { MDXComponents } from "mdx/types"
-import { MDXRemote } from "next-mdx-remote/rsc"
 
 import getStartup from "./get-startup"
 import getStartups from "../get-startups"
+import RemoteContent from "./remote-content"
+import LocalContent from "./local-content"
 
 export async function generateStaticParams() {
   const startups = getStartups()
@@ -21,6 +21,8 @@ export default async function Details({
   params: { id: string }
 }) {
   const startup = getStartup(id)
+  const theme = fr.getColors(false)
+  const blueCumulus = theme.decisions.background.alt.blueCumulus.default
 
   const {
     attributes: {
@@ -35,36 +37,6 @@ export default async function Details({
       content_url_encoded_markdown,
     },
   } = startup
-
-  const markdown = await remark().process(
-    decodeURIComponent(content_url_encoded_markdown)
-  )
-
-  const component = {
-    h1: ({ children }: { children: React.ReactNode }) => (
-      <h1 className="fr-h1">{children}</h1>
-    ),
-    h2: ({ children }: { children: React.ReactNode }) => (
-      <h2 className="fr-h2">{children}</h2>
-    ),
-    h3: ({ children }: { children: React.ReactNode }) => (
-      <h2 className="fr-h3">{children}</h2>
-    ),
-    p: ({ children }: { children: React.ReactNode }) => (
-      <p className="mb-6">{children}</p>
-    ),
-    ul: ({ children }: { children: React.ReactNode }) => (
-      <ul className="mb-6">{children}</ul>
-    ),
-    a: ({ children, ...props }: { children: React.ReactNode }) => (
-      <a {...props} target="_blank" rel="noopener">
-        {children}
-      </a>
-    ),
-  } as MDXComponents
-
-  const theme = fr.getColors(false)
-  const blueCumulus = theme.decisions.background.alt.blueCumulus.default
 
   return (
     <div className="startups-details">
@@ -116,11 +88,10 @@ export default async function Details({
         </div>
       </div>
 
-      <div className="fr-py-6w">
-        <div className="fr-container">
-          <MDXRemote source={markdown} components={component} />
-        </div>
-      </div>
+      <LocalContent id={id} />
+      <RemoteContent
+        content_url_encoded_markdown={content_url_encoded_markdown}
+      />
     </div>
   )
 }
