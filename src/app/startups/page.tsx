e232c753f16f092af7bec410b3ed525629c894e0
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2023-09-12 18:30:56 +0200

    various fixes

--- src/app/startups/page.tsx
+++ src/app/startups/page.tsx
@@ -1,11 +1,10 @@
-import { fr } from "@codegouvfr/react-dsfr"
 import StartupsList from "@/components/startups-list"
 
 export default function StartupsPage() {
   return (
-    <section className={fr.cx("fr-container", "fr-pb-6w")}>
-      <h1 className={fr.cx("fr-h1")}>Nos startups</h1>
+    <div className="startups-list fr-container">
+      <h1 className="fr-h1">Nos startups</h1>
       <StartupsList />
-    </section>
+    </div>
   )
 }
--- src/components/startups-list/index.tsx
+++ src/components/startups-list/index.tsx
@@ -4,29 +4,62 @@ import { useState } from "react"
 
 import Item from "./item"
 import Filters from "./filters"
-import getStartups from "@/utils/get-startups"
+import getStartups, { type Startup } from "@/utils/get-startups"
+
+type Titles = {
+  [key: string]: number | string
+}
+
+const titles: Titles = {
+  travail: "Travail",
+  "solidarite-sante": "Santé & Solidarité",
+}
+
+type groupedStartups = Record<keyof Titles, Startup[]>
+
+function getGroupTitle(domain: string) {
+  return titles[domain] ? titles[domain] : domain
+}
 
 export default function StartupsList() {
   const startups = getStartups()
   const [selectedPhase, setSelectedPhase] = useState<string>()
 
-  function handleClick(phase: string) {
-    setSelectedPhase(phase)
-  }
-
   const filteredStartups = selectedPhase
     ? startups.filter(
-        (startup) =>
-          startup.attributes.phases.slice(-1).pop()?.name === selectedPhase
+        ({ attributes: { phases } }) =>
+          phases.slice(-1).pop()?.name === selectedPhase
       )
     : startups
 
+  const groupedStartups = filteredStartups.reduce((groups, startup) => {
+    const {
+      attributes: {
+        organisation: { domaine_ministeriel },
+      },
+    } = startup
+
+    if (!groups[domaine_ministeriel]) {
+      groups[domaine_ministeriel] = []
+    }
+    groups[domaine_ministeriel].push(startup)
+
+    return groups
+  }, {} as groupedStartups)
+
   return (
     <div className="startups">
-      <Filters onClick={handleClick} selectedPhase={selectedPhase} />
-      <div className="startups-list grid grid-cols-4 gap-6 fr-mt-6w">
-        {filteredStartups.map((startup) => (
-          <Item key={startup.id} startup={startup} />
+      <Filters onClick={setSelectedPhase} selectedPhase={selectedPhase} />
+      <div className="list">
+        {Object.entries(groupedStartups).map(([domain, startups]) => (
+          <section key={`group-${domain}`} className="fr-mt-6w">
+            <h2 className="fr-h2">{getGroupTitle(domain)}</h2>
+            <div className="grid grid-cols-4 gap-6">
+              {startups.map((startup) => (
+                <Item key={startup.id} startup={startup} />
+              ))}
+            </div>
+          </section>
         ))}
       </div>
     </div>
