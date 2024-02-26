bb79eb40f82bcc545de070db1cd330833aa91eb9
Gary van Woerkens <gary.van-woerkens@sg.social.gouv.fr>
2024-02-26 15:46:57 +0100

    fix: update mentions legales (#1066)
    * fix: update mentions legales
    
    * chore: prettier

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -13,38 +13,44 @@ const Mentions = () => (
       <a href="https://fabrique.social.gouv.fr/">
         Fabrique des Ministères sociaux
       </a>{" "}
-      situé :
+      située :
       <br />
       <br />
       Tour Mirabeau <br />
       39-43 Quai André Citroën
       <br />
-      75015 PARIS
+      75739 Paris Cedex 15
+      <br />
+      France
+      <br />
       <br />
       Tél : 01 40 56 60 00
       <br />
+      <br />
       <h3>Directeur de la publication</h3>
-      Anne JeanJean - Directrice du Numérique
+      Madame Anne JEANJEAN, Directrice du numérique des ministères sociaux.
+      <br />
       <h3>Hébergement de la plateforme</h3>
-      Ce site est hébergé par Microsoft Azure France (région France centre) :
+      Cette plateforme est hébergée par :
       <br />
-      Microsoft France 37 Quai du Président Roosevelt 92130 ISSY-LES-MOULINEAUX
       <br />
-      <h3>À savoir</h3>
-      Le site ne récole ni ne traite aucune donnée à caractère personnel. La
-      mesure d&apos;audience (nombre de visites, pages consultées) est réalisée
-      par un outil libre intitulé <a href="https://matomo.org">Matomo</a>{" "}
-      spécifiquement paramétré, respectant les conditions d&apos;exemption du
-      consentement de l&apos;internaute définies par la{" "}
-      <a href="https://www.cnil.fr/fr/solutions-pour-la-mesure-daudienc">
-        recommandation « Cookies »
-      </a>{" "}
-      de la Commission nationale informatique et libertés (CNIL). Cela signifie,
-      notamment, que ces cookies ne servent qu&apos;à la production de
-      statistiques anonymes et ne permettent pas de suivre la navigation de
-      l&apos;internaute sur d&apos;autres sites. Vous pouvez tout de même
-      désactiver cet outil :
+      OVH SAS
       <br />
+      2 rue Kellermann
+      <br />
+      59100 Roubaix
+      <br />
+      France
+      <br />
+      <br />
+      <h3>Confidentialité</h3>
+      <p>
+        La Plateforme ne traite aucune donnée à caractère personnel et ne dépose
+        pas de cookie ou de traceurs. Toutefois, nous utilisons la solution de
+        mesure d’audience Matomo, configurée en mode « exempté » et ne
+        nécessitant pas le recueil de votre consentement conformément aux
+        recommandations de la CNIL.
+      </p>
       <iframe
         title="Matomo"
         style={{ border: 0, height: 200, width: "100%" }}
